2df6ea4c1c5ec396c480cf276924106d09d2e83d
Fatima-ezzahra <jeontima07@gmail.com>
2023-02-28 10:35:12 -0800

    task 0000

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,18 @@
-#include <main.h>
-int main(void) {
-	printf("_putchar\n");
+#include "main.h"
+/**
+ * main -print _putchar
+ * is written by mah
+ * return 0
+ */
+int main(void){
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return 0;
 }
-
 
